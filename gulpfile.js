6da1f8b9ef303f9f2f4b75bedecfb81b71e66616
Serhii Tsaruk <s0936771414@gmail.com>
2016-10-16 19:54:25 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 var gulp = require('gulp'),
 
     inlineCss = require('gulp-inline-css');
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect'),
 
+    // server connect
+    gulp.task('connect', function() {
+        connect.server({
+            root: 'build',
+            livereload: true
+        });
+    });
 
+    //inliner
+    gulp.task('inline', function() {
+        return gulp.src('src/*.html')
+            .pipe(inlineCss({
+                applyStyleTags: true,
+                applyLinkTags: true,
+                removeStyleTags: true,
+                removeLinkTags: true,
+                preserveMediaQueries: true
+            }))
+            .pipe(gulp.dest('build/'));
+    });
 
-gulp.task('inline', function() {
-    return gulp.src('src/*.html')
-        .pipe(inlineCss({
-            applyStyleTags: true,
-            applyLinkTags: true,
-            removeStyleTags: true,
-            removeLinkTags: true,
-            preserveMediaQueries: true
-        }))
-        .pipe(gulp.dest('build/'));
-});
+    //watch
+    gulp.task('watch', function () {
+        gulp.watch('src/css/*.css', ['css'])
+        gulp.watch('src/*.html', ['html'])
+    });
+    
+    //default
+    gulp.task('default', ['connect', 'inline', 'watch']);
